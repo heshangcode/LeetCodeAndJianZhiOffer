363a47687f84842397f6665739867b3a01470c03
heshangcode <914727235@qq.com>
2019-09-18 20:58:20 +0800

    小修改

--- src/com/hs/LeetCode/链表/LeetCode86分隔链表Solution2.java
+++ src/com/hs/LeetCode/链表/LeetCode86分隔链表Solution2.java
@@ -15,42 +15,32 @@ import com.hs.LeetCode.ListNode;
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/partition-list
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
- *
+ * <p>
  * 思路1：遍历链表，当前节点值 > x，给他放到最后一个节点的后面
  *
  * @Author heshang.ink
  * @Date 2019/8/10 17:34
  */
-public class LeetCode86分隔链表 {
+public class LeetCode86分隔链表Solution2 {
 	public ListNode partition(ListNode head, int x) {
 		if (head == null) {
 			return null;
 		}
-		ListNode dummy = new ListNode(0);
-		dummy.next = head;
-		//最后节点的位置
-		ListNode lastNode = head;
-		ListNode cur = dummy;
-		//找到最后节点的位置
-		while (lastNode.next != null) {
-			lastNode = lastNode.next;
-		}
-		//这里是为了记录最后一个节点，与cur指针遍历到这个节点就停止
-		ListNode last = lastNode;
-		while (cur != null && cur.next != null && last != cur) {
-			//把当前遍历的节点 > x，放到最后去
-			if (cur.next.val >= x) {
-				ListNode next = cur.next;
-				ListNode nextNext = cur.next.next;
-				next.next = null;
-				lastNode.next = next;
-				lastNode = next;
-				cur.next = nextNext;
-				cur = nextNext;
+		ListNode min = new ListNode(0);
+		ListNode minCur = min;
+		ListNode max = new ListNode(0);
+		ListNode maxCur = max;
+		while (head != null) {
+			if (head.val < x) {
+				minCur.next = new ListNode(head.val);
+				minCur = minCur.next;
 			} else {
-				cur = cur.next;
+				maxCur.next = new ListNode(head.val);
+				maxCur = maxCur.next;
 			}
+			head = head.next;
 		}
-		return dummy.next;
+		minCur.next = max.next;
+		return min.next;
 	}
 }
