01a8a3444b1fa5eadb1580445ceb3ce24e6f6196
heshangcode <914727235@qq.com>
2019-10-17 13:14:51 +0800

    修改一些

--- src/com/hs/LeetCode/DP/LeetCode300最长上升子序列.java
+++ src/com/hs/LeetCode/DP/LeetCode300最长上升子序列.java
@@ -3,7 +3,16 @@ package com.hs.LeetCode.DP;
 import java.util.Arrays;
 
 /**
- * TODO
+ * 给定一个无序的整数数组，找到其中最长上升子序列的长度。
+ *
+ * 示例:
+ *
+ * 输入: [10,9,2,5,3,7,101,18]
+ * 输出: 4
+ * 解释: 最长的上升子序列是 [2,3,7,101]，它的长度是 4。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-increasing-subsequence
  *
  * @Author heshang.ink
  * @Date 2019/9/16 12:33
--- src/com/hs/LeetCode/树/LeetCode98验证二叉搜索树.java
+++ src/com/hs/LeetCode/树/LeetCode98验证二叉搜索树.java
@@ -1,7 +1,35 @@
 package com.hs.LeetCode.树;
 
 /**
- * https://leetcode-cn.com/problems/validate-binary-search-tree/
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ *
+ * 假设一个二叉搜索树具有如下特征：
+ *
+ * 节点的左子树只包含小于当前节点的数。
+ * 节点的右子树只包含大于当前节点的数。
+ * 所有左子树和右子树自身必须也是二叉搜索树。
+ * 示例 1:
+ *
+ * 输入:
+ *     2
+ *    / \
+ *   1   3
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入:
+ *     5
+ *    / \
+ *   1   4
+ *      / \
+ *     3   6
+ * 输出: false
+ * 解释: 输入为: [5,1,4,null,null,3,6]。
+ *      根节点的值为 5 ，但是其右子节点值为 4 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/validate-binary-search-tree
+ *
  * 思路：二叉搜索树的定义，左子树小于根，右子树大于根
  * 乍一看，这是一个平凡的问题。只需要遍历整棵树，检查 node.right.val > node.val 和
  * node.left.val < node.val 对每个结点是否成立。
